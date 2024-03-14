a2ba554df5b7055a266e3a99214c54b54aff770b
srirakeshv <srirakeshv@gmail.com>
2024-03-14 17:27:18 +0530

    updated the hamburger menu and submenu code

--- src/Components/Homepage/MobileItems.jsx
+++ src/Components/Homepage/MobileItems.jsx
@@ -1,24 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MobileItems = () => {
+  const [active, setactive] = useState({ overall: false, index: null });
+  const [mainactive, setmainactive] = useState(true);
+
   const Items = [
     {
       Title: "Home",
     },
     {
       Title: "Services",
+      submenu: [
+        {
+          name: "Back",
+          path: "",
+        },
+        {
+          name: "Who We Are",
+          path: "",
+        },
+        {
+          name: "What We Do",
+          path: "",
+        },
+      ],
     },
     {
       Title: "Comapany",
+      submenu: [
+        {
+          name: "Back",
+          path: "",
+        },
+        {
+          name: "company 1",
+          path: "",
+        },
+        {
+          name: "company 2",
+          path: "",
+        },
+      ],
     },
   ];
+
+  const handleclick = (index) => {
+    if (index === 0) {
+    } else {
+      setactive({ overall: !active.overall, index });
+      setmainactive(false);
+    }
+  };
+
+  const handlesubmenuclick = (index) => {
+    setactive(index === 0 ? { overall: false } : null);
+    setmainactive(true);
+  };
+
   return (
-    <div className="w-44 p-3 bg-red-300 absolute top-12 left-5 z-10">
-      <ul className="flex flex-col justify-start items-start gap-3 w-full">
-        {Items.map((item, index) => (
-          <li key={index}>{item.Title}</li>
-        ))}
-      </ul>
+    <div className="w-44 p-3 bg-red-500 absolute top-12 left-12 z-10">
+      {mainactive && (
+        <ul className="flex flex-col justify-start items-start gap-3 w-full">
+          {Items.map((item, index) => (
+            <li
+              className="cursor-pointer"
+              key={index}
+              onClick={() => handleclick(index)}
+            >
+              {item.Title}
+            </li>
+          ))}
+        </ul>
+      )}
+      {active.overall && (
+        <ul className="bg-rose-300">
+          {Items[active.index].submenu &&
+            Items[active.index].submenu.map((subItem, subIndex) => (
+              <li
+                key={subIndex}
+                className="cursor-pointer"
+                onClick={() => handlesubmenuclick(subIndex)}
+              >
+                {subItem.name}
+              </li>
+            ))}
+        </ul>
+      )}
     </div>
   );
 };
